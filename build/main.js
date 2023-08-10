7f935f4ddc6e3c5ec16a40bc4659e80d8eb78fd6
Anders Boyum <Acboyum@gmail.com>
2023-08-10 12:33:02 -0500

    trying to figure out why I can't use transition end for css transition magic

--- build/main.js
+++ build/main.js
@@ -1,8 +1,25 @@
 "use strict";
 window.addEventListener('keydown', keyPress);
+// KeyboardEvent type is for general purpose user-keyboard-interactions, seems
 function keyPress(e) {
+    // There are a lot of HTML element types available for use in TS
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!audio)
         return;
+    audio.currentTime = 0; // resets audio to start each keypress, allowing multiple successive keypresses 
     audio.play();
+    changeClassToPlaying(e);
+    // changeClassToNotPlaying(e);
 }
+// function to toggle on the 'playing' class on the button pressed, then toggle off after set interval
+const changeClassToPlaying = (e) => {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    key === null || key === void 0 ? void 0 : key.classList.toggle('playing');
+};
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => { key.addEventListener('transitionend', removeTransition); });
+const removeTransition = (e) => {
+    console.log(e);
+};
+// const changeClassToNotPlaying = (e: KeyboardEvent) => {
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 window.addEventListener('keydown', keyPress);
 
+// KeyboardEvent type is for general purpose user-keyboard-interactions, seems
 function keyPress(e: KeyboardEvent) {
+    // There are a lot of HTML element types available for use in TS
     const audio: HTMLAudioElement | null = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 
     if(!audio) return;
 
+    audio.currentTime = 0; // resets audio to start each keypress, allowing multiple successive keypresses 
     audio.play();
+    changeClassToPlaying(e);
+    // changeClassToNotPlaying(e);
 }
+
+// function to toggle on the 'playing' class on the button pressed, then toggle off after set interval
+const changeClassToPlaying = (e: KeyboardEvent) => {
+    const key: HTMLElement | null= document.querySelector(`.key[data-key="${e.keyCode}"`);
+    key?.classList.toggle('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {key.addEventListener('transitionend', removeTransition)})
+
+const removeTransition = (e) => {
+    console.log(e)
+}
+
+// const changeClassToNotPlaying = (e: KeyboardEvent) => {
+// }
