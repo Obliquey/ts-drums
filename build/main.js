91d9a145d413772281d1e8854d93f5da8e9c005b
Anders Boyum <Acboyum@gmail.com>
2023-08-10 11:43:28 -0500

    was able to access audio wav files upon keypress

--- build/main.js
+++ build/main.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('Hello World!');
+window.addEventListener('keydown', keyPress);
+function keyPress(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio)
+        return;
+    audio.play();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+window.addEventListener('keydown', keyPress);
+
+function keyPress(e: KeyboardEvent) {
+    const audio: HTMLAudioElement | null = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+    if(!audio) return;
+
+    audio.play();
+}
